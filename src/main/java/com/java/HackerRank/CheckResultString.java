c393e1505b27151b679e6b82adfdff2c86e62474
vahuja <vahuja@1worldsync.com>
2022-10-16 10:24:41 +0530

    FSE Program

---
+++ src/main/java/com/java/HackerRank/CheckResultString.java
@@ -0,0 +1,2 @@
+package com.java.HackerRank;public class CheckResultString {
+}
---
+++ src/main/java/com/java/HackerRank/PrintChar.java
@@ -0,0 +1,18 @@
+package com.java.PrintCHar;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class PrintChar {
+
+    public static void main(String[] args) {
+        String s = "vipinahuja";
+
+      //  Map<Character,Integer> mapChar = s.chars().mapToObj(c->(char)c).collect(Collectors.toMap(Function.identity()));
+
+    }
+}
